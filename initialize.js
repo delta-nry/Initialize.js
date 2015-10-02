265769d571df9e80462bb339c0476d8b1f4e3516
Nathan Yee <ny.nathan.yee@gmail.com>
2015-10-02 16:02:20 -0700

    Check that init is a function

--- initialize.js
+++ initialize.js
@@ -4,7 +4,7 @@ var i = (function() {
     c: function(obj, args) {
       if (arguments.length > 0) {
         var newObj = Object.create(arguments[0]);
-        if ('init' in newObj) {
+        if ('init' in newObj && typeof newObj.init === 'function') {
           if (arguments.length > 1) {
             var args = [];
             for (var i = 1; i < arguments.length; i++) {
